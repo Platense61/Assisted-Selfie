7328f6e9e18bd0e2befff0b2e4923df827fd7046
Platense61 <55705292+Platense61@users.noreply.github.com>
2022-04-15 12:07:15 -0400

    basic webcam integration and face tracking

--- main.py
+++ main.py
@@ -1,36 +1,24 @@
 import cv2
-import sys
+face_cascade = cv2.CascadeClassifier(r'C:/Users/rcanv/Documents/web/Assisted-Selfie/haarcascade_frontalface_default.xml') 
 
-#cascPath = sys.argv[1]
-#faceCascade = cv2.CascadeClassifier(cascPath)
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
 
-video_capture = cv2.VideoCapture(0)
-
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
-
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+while True:  
+    ret, img = cap.read()  
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    faces = face_cascade.detectMultiScale(gray_img, 1.25, 4) 
+  
+    for (x,y,w,h) in faces: 
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+        rec_gray = gray_img[y:y+h, x:x+w] 
+        rec_color = img[y:y+h, x:x+w]   
+  
+    cv2.imshow('Face Recognition',img) 
+  
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: 
         break
-
-# When everything is done, release the capture
-video_capture.release()
+  
+cap.release() 
 cv2.destroyAllWindows()
+
